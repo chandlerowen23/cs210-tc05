e1cc70781dcf93206ada828645ba006a6b77039d
Chantheman23 <74678113+chandlerowen23@users.noreply.github.com>
2021-02-04 21:23:31 -0700

    Update killer.py

--- jumper_game/jumper/game/killer.py
+++ jumper_game/jumper/game/killer.py
@@ -1,12 +1,17 @@
 import random
 
+#class for printing out the parachute
 class Killer():
 
+    #initialize the attribute of the killer class
     def __init__(self):
+        #list of words for jumper to randomize
         self.list_word =["house", "table", "rock", "guesser"]
+        #random choice from the list of worder stored into word
         self.word = random.choice(self.list_word)
 
 
+    #depending on how many fails or mistakes the print of the parachute will change
     def get_fails(self,fails):
 
         if fails >= 4:
